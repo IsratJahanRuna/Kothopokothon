4afb75b9524e93dc8fff342b68ad27b0c843eaef
meherinakter13 <meherinakter13@gmail.com>
2021-05-29 22:31:04 +0600

    sidebar and message box

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import KMessage from './KMessage';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-     <h1>this is prantik</h1>
-=======
-   <h1>HI....</h1>
->>>>>>> 7eeed646623f2e49797423070d18121f1c728db3
+      <KMessage></KMessage>
+
+      
+
     </div>
   );
 }

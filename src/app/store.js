6bddd0924c88a7c889e170ac5337cf7fbc761516
Nishat009 <nishatraihana009@gmail.com>
2021-05-30 11:18:26 +0600

    working on chatSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/counter/userSlice';
-
+import chatReducer from '../features/counter/chatSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    chat:chatReducer,
   },
 });
